cb170051d467a29fe36ba6e95de95b53356c615f
G.Shubha Sri <G.Shubha Sri@LAPTOP-DCLA9RQ3>
2021-03-06 13:11:52 +0530

    vhuhguy

---
+++ Flipkart/src/main/java/com/flipkart/pages/LoginPage.java
@@ -0,0 +1,31 @@
+package com.flipkart.pages;
+	
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.support.FindBy;
+	import org.openqa.selenium.support.PageFactory;
+    import com.flipkart.genericLib.BaseTest;
+
+
+	public class LoginPage {
+		@FindBy(name="username") private WebElement untb;
+		@FindBy(name="pwd") private WebElement pwtb;
+		@FindBy(xpath="//input[@type='submit']") private WebElement loginBtn;
+		
+		public LoginPage()
+		{
+			PageFactory.initElements(BaseTest.driver,this);
+		}
+		
+		public void login(String un,String pwd)
+		{
+			untb.sendKeys(un);
+			pwtb.sendKeys(pwd);
+			loginBtn.click();
+		}
+		
+		
+		
+
+	}
+
+
