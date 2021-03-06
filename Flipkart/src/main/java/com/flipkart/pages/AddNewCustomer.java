1c37afb0450dcb7d05c71d0c8b140b452dce69ce
G.Shubha Sri <G.Shubha Sri@LAPTOP-DCLA9RQ3>
2021-03-06 12:42:22 +0530

    dijofjiodf

---
+++ Flipkart/src/main/java/com/flipkart/pages/AddNewCustomer.java
@@ -0,0 +1,30 @@
+package com.flipkart.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.flipkart.genericLib.BaseTest;
+
+public class AddNewCustomer 
+{
+
+	@FindBy(xpath="//input[@name='name']") private WebElement customername;
+	@FindBy(xpath="////input[@name='createCustomerSubmit']") private WebElement createcustomerbtn;
+	
+	public AddNewCustomer()
+	{
+		PageFactory.initElements(BaseTest.driver,this);
+	}
+	public void createcustomer(String name)
+	{
+		customername.sendKeys(name);
+	}
+	public void customerbtn()
+	{
+		createcustomerbtn.click();
+	
+	
+	
+	}
+}
